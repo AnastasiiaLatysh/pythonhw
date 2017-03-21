be3b10d89c763ed05f29155b0ca934e11dffcd94
Anastasiia Bondarenko <anastasia@localhost.localdomain>
2017-03-21 20:45:38 +0200

    function diagonal_reverse(matrix) was added

---
+++ basics/7.py
@@ -0,0 +1,16 @@
+def diaginal_reverse(matrix) :
+    if not matrix: return []
+    return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+
+def print_matrix(matrix) :
+    for i in range(len(matrix)):
+        print(matrix[i])
+
+matrix = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9]]
+print_matrix(matrix)
+print()
+
+matrix = diaginal_reverse(matrix)
+print_matrix(matrix)
