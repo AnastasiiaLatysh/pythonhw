e506cc105a99263c547974b1a225c7cb358b9c6e
mkoshtc <nikolay.koshraniy@gmail.com>
2017-03-21 21:29:29 +0200

    Use  time.sleep(sec) in task5 for better presentation result

--- basics/task5.py
+++ basics/task5.py
@@ -1,7 +1,9 @@
+import time
 def histogram(arr):
     for integerVal in arr:
         print('*'*integerVal)
+        time.sleep(1)
 
-arr = [4, 9, 7, 11, 1, 5];
+arr = [4, 9, 7, 11, 1, 5, 12, 3, 5];
 print("Our array", arr)
 histogram(arr)
