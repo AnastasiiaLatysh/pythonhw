4576d0410cbbde834418c213afc5da7a45af9eed
olenakhom <olena.khom@gmail.com>
2017-03-21 20:05:13 +0200

    reversal of string function

---
+++ basics/hw1/task3.py
@@ -0,0 +1,6 @@
+# 3. Define a function `reverse()` that computes the reversal of a string. For example, `reverse("I am testing")` should return the string "gnitset ma I".
+
+def reverse(text):
+    return text[::-1]
+
+print(reverse('I am testing'))
