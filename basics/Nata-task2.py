b23191f18ef5c83de91c2624fef0dd384ed8508d
nklymenok <nata_klimenok@ukr.net>
2017-03-21 20:19:07 +0200

    My second function

---
+++ basics/Nata-task2.py
@@ -0,0 +1,17 @@
+
+def sum(number):
+    summ = 0
+    for number in list:
+        summ += number
+    print(summ)
+
+def multiply(number):
+    mul=1
+    for number in list:
+        mul *=number
+    print(mul)
+
+
+list = [1,2,3,4]
+sum(list)
+multiply(list)
