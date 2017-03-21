d7a813b2d7a1ef769cf2f174e536a34c09dfa71f
Anastasiia Bondarenko <anastasia@localhost.localdomain>
2017-03-21 21:04:34 +0200

    function game(A, B) was added

---
+++ basics/8.py
@@ -0,0 +1,26 @@
+import random
+
+def game(A, B) :
+    random_number = random.randint(A, B)
+    print(random_number)
+
+    userWon = False
+    while not userWon :
+        num = int(input('Guess the number :'))
+
+        while not num :
+            num = int(input('Guess the number :'))
+
+        if random_number == num :
+            print("Congratulations!")
+            userWon = True
+        else:
+            print("Try again...")
+
+
+
+
+first = int(input('Enter range : \nfrom = '))
+last = int(input('to = '))
+
+game(first, last)
